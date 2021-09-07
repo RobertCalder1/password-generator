716a95d056ec567e8de718ea4f27772370048cfb
RobertCalder1 <robertcalder27@hotmail.com>
2021-09-07 18:32:02 +1000

    stylise page and change 'password-length' name and classes to 'characters'

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
-// slider scripting
-var slider = document.getElementById("Password-Length");
-var output = document.getElementById("length-value");
+// slider output scripting
+var slider = document.getElementById("Characters");
+var output = document.getElementById("characters-value");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
